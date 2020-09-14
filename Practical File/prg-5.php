0a060ff87a637b36b02e61c4a7f2d1d44075c967
Rohitc2001 <rohit2001@outlook.com>
2020-09-14 11:45:06 +0530

    Comparison Operators

---
+++ Practical File/prg-5.php
@@ -0,0 +1,39 @@
+<?php
+
+// WAP Comparison Operators
+
+$a = 10 ;
+$b = '10';
+$c = 10;
+$d = 5;
+
+// Equal example
+var_dump($a == $b );
+echo"<br>";
+var_dump($a == $c);
+echo"<br><br>";
+
+//Identical  
+var_dump($a === $b );
+echo"<br>";
+var_dump($a === $c);
+echo"<br><br>";
+
+//Not equal example
+var_dump($a != $b );
+echo"<br>";
+var_dump($a == $c);
+echo"<br><br>";
+
+//Not identical example
+var_dump($a !== $b );
+echo"<br>";
+var_dump($a == $c);
+echo"<br><br>";
+
+//Concatentoion assignment
+$str1 = "Svvv";
+$str2 = "Indore";
+
+echo $str1 .= $str2;
+?>
