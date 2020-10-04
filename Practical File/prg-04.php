76e0d2ecb25656cbf9c888196b2b07e47c6f3fe3
Rohitc2001 <rohit2001@outlook.com>
2020-10-04 22:29:53 +0530

    Arithemetic operators

---
+++ Practical File/prg-04.php
@@ -0,0 +1,26 @@
+<?php 
+// WAP Arithametic operators
+$a = 10;
+$b = 5;
+
+echo "a = 10 <br> b = 5 <br><br>";
+
+$sum = $a + $b;
+echo "<b>Addition of a & b :</b> <br> $a + $b = $sum  <br>";
+
+$sub = $a - $b;
+echo "<b>Difference of a & b :</b> <br> $a - $b = $sub  <br>";
+
+$mul = $a * $b;
+echo "<b>Multiplication of a & b :</b> <br> $a + $b = $mul <br>";
+
+$div = $a / $b;
+echo "<b>Division of a & b :</b> <br> $a / $b = $div <br>";
+
+$mod = $a % $b;
+echo "<b>mod of a & b :</b><br>$a % $b = $mod <br>";
+
+$pow = $a ** $b;
+echo "<b>power :</b><br>$a ** $b is = $pow";
+
+?>
