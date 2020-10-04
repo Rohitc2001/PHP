30c9ace5c1707d30144b0000384de12fd20e2f55
Rohitc2001 <rohit2001@outlook.com>
2020-10-04 22:38:13 +0530

    Fibonacci Series

---
+++ Practical File/prg-07.php
@@ -0,0 +1,24 @@
+
+<?php 
+
+// Program for Fabonic series.
+
+$count = 0;
+
+$f1 = 0;
+$f2 = 1;
+
+echo $f1." , ".$f2." ";
+
+while($count < 15)
+{
+    $sum = $f1 + $f2;
+
+    echo " , ".$sum;
+
+    $f1 = $f2;
+    $f2 = $sum;
+
+    $count++; 
+}
+?>
